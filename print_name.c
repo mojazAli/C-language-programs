ca45e742fb7ead6d4ee79f1bc7924208701476d5
mojazAli <mojazali9976@gmail.com>
2025-02-20 22:05:30 +0500

    program 2

---
+++ print_name.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("Ahmad");
+	getch(); 
+	// C language is a case-sensitive language
+	/* Which means (a) is different from (A).
+	
+	Printf() Capital P, is different from 
+	printf() Small p */
+	printf("Ayaz");
+	
+	getch();
+	return 0;
+}
